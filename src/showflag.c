bea1b14feab8d0e81d3c6d01800fbbdfc3d7a3a8
Luke Sheppard <tcpiplab@users.noreply.github.com>
2015-10-22 00:40:27 -0700

    First commit. From CloudPebble IDE.

---
+++ src/showflag.c
@@ -0,0 +1,54 @@
+/*
+ * main.c
+ * Sets up a Window and BitmapLayer, then loads and displays a PNG as a GBitmap
+ */
+
+#include <pebble.h>
+
+static Window *s_main_window;
+static GBitmap *s_bitmap;
+static BitmapLayer *s_bitmap_layer;
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_US_FLAG_IMAGE);
+
+  s_bitmap_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+#if defined(PBL_BW)
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpAssign);
+#elif defined(PBL_COLOR)
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
+#endif
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+}
+
+static void main_window_unload(Window *window) {
+  bitmap_layer_destroy(s_bitmap_layer);
+  gbitmap_destroy(s_bitmap);
+}
+
+static void init() {
+  s_main_window = window_create();
+#ifdef PBL_SDK_2
+  window_set_fullscreen(s_main_window, true);
+#endif
+  //window_set_background_color(s_main_window, COLOR_FALLBACK(GColorBlue, GColorBlack));
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main() {
+  init();
+  app_event_loop();
+  deinit();
+}
